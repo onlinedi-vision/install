cfc8d7b675dbdbe171f71b47dd5544b61ea8c3d3
OllieOlafsson <francesco.xurea@yahoo.com>
2025-08-29 09:57:33 +0300

    latest release pull using github api + fixes

--- i.sh
+++ i.sh
@@ -3,7 +3,11 @@ set -e
 
 echo "  * Fetching AppImage.tar.gz (/tmp/division-client-install-tmp.tar.gz)"
 
-wget -O /tmp/division-client-install-tmp.tar.gz https://github.com/onlinedi-vision/od-client/releases/download/v0.0.2-alpha/division_client_002.tar.gz
+curl -s https://api.github.com/repos/onlinedi-vision/od-client/releases/latest \
+	| grep "browser_download_url" \
+	| awk -F ' ' '{ print $2 }' \
+	| tr -d \" \
+	| wget -O /tmp/division-client-install-tmp.tar.gz -qi - --show-progress 
 
 
 echo "\n\n\n  * Creating /opt/division-online directory (if not existant)"
@@ -15,9 +19,11 @@ echo "  * Untaring appimage (into /opt/division-online)"
 sudo tar -zvxf /tmp/division-client-install-tmp.tar.gz -C /opt/division-online
 sudo chmod 0777 /opt/division-online/division.client.AppDir/
 
+# Added full permissions to 'other' so echo commands can execute successfully; could revise
 echo "  * Creating /usr/bin/division-online-client script"
-sudo chmod o+x /usr/bin/division-online-client
-sudo echo "#!/bin/bash" > /usr/bin/division-online-client
+sudo touch /usr/bin/division-online-client
+sudo chmod o+wx /usr/bin/division-online-client
+sudo echo "#!/usr/bin/env bash" > /usr/bin/division-online-client
 sudo echo "/opt/division-online/division.client.AppDir/AppRun" >> /usr/bin/division-online-client
 
 if [ ! -d $HOME/.division-online ]; then
